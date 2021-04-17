f9c5a1828bb7c0a2a7a94ed600a3f0f4485f976f
Vang Huynh <vanghm@imt-soft.com>
2021-04-17 18:11:03 +0700

    add project

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        "Hello, my name is Bo",
+        style: TextStyle(fontSize: 30),
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
